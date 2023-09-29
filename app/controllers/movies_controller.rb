8bba7b0828a86c3331c82c2ba06f98fe779e0868
Angela Zhang <zz2921@columbia.edu>
2023-09-29 20:10:32 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.all 
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = []
+
+    if params.has_key?(:ratings)
+      @movies = Movie.with_ratings(params[:ratings].keys)
+    end
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    # return Movie.distinct.pluck(:rating)
+    return ['G','PG','PG-13','R'] 
+  end 
+
+  def self.with_ratings(ratings)
+    # takes an array of ratings (e.g. ["r", "pg-13"]) and 
+    # returns an ActiveRecord relation of movies whose rating matches (case-insensitively) 
+    # anything in that array
+    return Movie.where(rating: ratings)
+  end
 end
